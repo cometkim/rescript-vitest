9aeb4c93a8cc0f6539d36c2154e6f1c3c2960377
Hyeseong Kim <hey@hyeseong.kim>
2025-05-24 21:09:27 +0900

    Temporarily disable vite-plugin-rescript
    See https://github.com/jihchi/vite-plugin-rescript/issues/231

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,10 @@ import rescript from '@jihchi/vite-plugin-rescript';
 
 export default defineConfig({
   test: {
-    includeSource: ['src/insource.mjs'], 
+    includeSource: ['src/insource.mjs'],
   },
   plugins: [
-    rescript(),
+    // https://github.com/jihchi/vite-plugin-rescript/issues/231
+    // rescript(),
   ],
 })
